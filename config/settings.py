ecab4e4b3ab30720032d5b4a2ddf44132d1af9da
Quokka <kminu616@gmail.com>
2024-12-03 13:38:39 +0900

    1.0

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = mysettings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['trandscope.onrender.com']
+ALLOWED_HOSTS = ['trandscope.onrender.com', 'localhost']
 
 
 # Application definition
